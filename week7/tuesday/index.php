23ed8ac8d0c93d98e6f3722f0299c00d3f483a38
David Derrick Anyuru <davidderrickanyuru@gmail.com>
2023-01-17 20:04:37 +0300

    While do while loop

--- week7/tuesday/index.php
+++ week7/tuesday/index.php
@@ -10,22 +10,62 @@
 <body>
    <section class="container">
         <h1>Operators session 4</h1>
-        <h3></h3>
+        <h3>Greater than operator</h3>
         <p>
             <?php 
-                $november_profit = 75000; // november profit
-                $december_profit = 120000; // december profit
+                $november_profit = 750000; // november profit
+                $december_profit = 1200000; // december profit
+
+                // The symbol in the line below 
             ?>
-            Is <?php $november_profit ?> >= <?php $december_profit?>
-            Is 5 === '5' ?
+            <?= 'Is November profit '. $november_profit. 'Greater or Equal to (>=) December profit'. $december_profit; ?>
+            <!-- The line above is an alternative for the line below. -->
+            Is <?= $november_profit ?> >= <?php $december_profit?> ?
             <span class="answer">
                 <?php var_dump($november_profit >= $december_profit); ?>
             </span>
         </p>
-        <p class="description">
-            The reason the above gives false is because the integer 5 at the left of the expression is not equivalent to the string on the right of the equivalent operator
+
+        <h3>
+            Ternary operator ( condition ? statement if the condition is true : statement if the condition is false) <br /> 
+            Inline conditional statement.
+        </h3>
+        <p>Assuming profit breakdown is: - <br/>
+            November 2022: <?php $november_profit?>
+            December 2022: <?php $december_operator?>
         </p>
-        
+        <p> 
+            <?= $november_profit > $december_profit ? 'Business performed better in November' : 'Business performed better in December'?>
+        </p>
+        <p> 
+            <?= $november_profit > $december_profit ? 'Business performed better in November' : 'Business was not good in november'?>
+        </p>
+        <?php 
+            $student_total_marks = 300;
+            $pass_mark = 350;
+        ?>
+        <p>
+            Students Total Marks: <?= $student_total_marks; ?><br/>
+            Pass Mark: <?= $pass_mark; ?>
+        </p>
+        <p>
+            <?= $student_total_marks >= $pass_mark ? 'Promoted to the next session' : 'Please try again'; ?>
+        </p>
+        <?php 
+            $temperature_today = 26;
+            $temperature_yesterday = 28;
+            $result = false;
+
+            $result = $temperature_today >= $temperature_yesterday ?  "Today is dead hot" : "Yesterday was hot like hell!"
+        ?>
+        <p>
+           Yesterday was: <?= $temperature_yesterday?> degrees celcius
+           <br/>
+           Today is: <?= $temperature_today?> degrees celcius 
+        </p>
+        <p>
+            <?= $result?>
+        </p>
    </section>
 </body>
 </html>
