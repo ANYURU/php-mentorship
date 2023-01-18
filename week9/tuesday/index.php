e64e2aa31d080e10ca581eab37b0333e7aab90fd
David Derrick Anyuru <davidderrickanyuru@gmail.com>
2023-01-18 09:46:13 +0300

    For loop

--- week9/tuesday/index.php
+++ week9/tuesday/index.php
@@ -14,43 +14,43 @@
 
     for ($number_of_students = 5; $number_of_students > 0; $number_of_students--) {
         echo "<p>This is my name. <br /> I am  student number {$number_of_students} in the queue.</p>";
-        if( $number_of_students === 3) {
+        if( $number_of_students === 3):
             break;
-        }h
+        endif;
     }
 
-    for ($customers_in_queue = 0; $customers_in_queue <br 5; $customers_in_queue++) {
+    for ($customers_in_queue = 0; $customers_in_queue < 5; $customers_in_queue++) {
         echo "<p>This is my name. <br /> I am  student number {$customers_in_queue} in the queue.</p>";
     }
 
-    for ($week_days = 1; $week_days <br 8; $weekdays++):
+    for ($week_days = 1; $week_days < 8; $weekdays++):
         switch($week_days):
             case 1:
-                echo "{$week_day} Sunday </br>";
+                echo "{$week_days} Sunday </br>";
                 break;
 
             case 2:
-                echo "{$week_day} Monday </br>";
+                echo "{$week_days} Monday </br>";
                 break;
             
             case 3:
-                echo "{$week_day} Tuesday </br>";
+                echo "{$week_days} Tuesday </br>";
                 break;
 
             case 4:
-                echo "{$week_day} Wednesday </br>";
+                echo "{$week_days} Wednesday </br>";
                 break;
 
             case 5:
-                echo "{$week_day} Thursday </br>";
+                echo "{$week_days} Thursday </br>";
                 break;
 
             case 6:
-                echo "{$week_day} Friday </br>";
+                echo "{$week_days} Friday </br>";
                 break;
 
             case 7:
-                echo "{$week_day} Saturday </br>";
+                echo "{$week_days} Saturday </br>";
                 break;
                 
 
