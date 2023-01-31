306e3c6ece57f8acbd9d11a7d7e6b62efb117407
David Derrick Anyuru <davidderrickanyuru@gmail.com>
2023-01-31 21:20:49 +0300

    Arrays PHP session 4

---
+++ week10/saturday/index.php
@@ -0,0 +1,21 @@
+<!Doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>Master Arrays in PHP Master class 3</title>
+  <meta name="description" content="A simple HTML5 Template for new projects.">
+  <meta name="author" content="SitePoint">
+  <link rel="stylesheet" href="css/styles.css?v=1.0">
+</head>
+
+<body>
+  <?php 
+    $shop = [
+
+    ]
+  ?>
+</body>
+</html>
---
+++ week11/tuesday/index.php
@@ -0,0 +1,19 @@
+<!Doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>Master Arrays in PHP Master class 4</title>
+  <meta name="description" content="A simple HTML5 Template for new projects.">
+  <meta name="author" content="SitePoint">
+  <link rel="stylesheet" href="css/styles.css?v=1.0">
+</head>
+
+<body>
+  <?php 
+   
+  ?>
+</body>
+</html>
