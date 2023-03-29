1bad202ee49c6cebbf9d8e24e114de46359b6486
David Derrick Anyuru <davidderrickanyuru@gmail.com>
2023-03-29 10:06:30 +0300

    Setting up tailwind css

---
+++ level-2/week-4/tuesday/app/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="./css/style.css">
+    <title>Document</title>
+</head>
+<body class="bg-black">
+    <h1 class="text-4xl text-green-200 text-center">Welcome to tailwind CSS in PHP</h1>
+    <div class="bg-blue-200 h-20 w-20"></div>
+</body>
+</html>
