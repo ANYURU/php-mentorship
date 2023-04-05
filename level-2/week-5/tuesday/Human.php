40684fbaaaab8ea98f8ce4a53ad0da4eb0d026d7
David Derrick Anyuru <davidderrickanyuru@gmail.com>
2023-04-05 09:50:57 +0300

    classes properties, methods and the object operator php.

---
+++ level-2/week-5/tuesday/Human.php
@@ -0,0 +1,49 @@
+<?php 
+/**
+ * What shall be covered?
+ * 1. What is OOP?
+ * 2. Classes vs Objects
+ * 3. Constructor vs Destructor
+ * 
+ */
+/**
+ * Syntax:  
+ * class Class_Name {
+ *  Properties
+ *  Methods
+ * }
+*/
+
+ 
+class Human {
+
+    public $name;
+
+    function set_name($name) {
+        $this->name = $name;
+    }
+
+    function get_name() {
+        return $this->name;
+    }
+}
+
+// Using the class( template )
+
+$fanny = new Human();
+$fanny->set_name("Fanny Nyayic");
+echo $fanny->name;
+echo "\n";
+
+$henry = new Human();
+$henry->set_name("Henry Onyango");
+echo $henry->name;
+
+$david = new Human();
+$david->set_name("David Derrick Anyuru");
+echo $david->get_name();
+
+
+
+
+?>
