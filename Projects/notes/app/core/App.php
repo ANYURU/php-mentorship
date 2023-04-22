3f5f5be2af67e303afd0fade72e31aa5908b5811
David Derrick Anyuru <davidderrickanyuru@gmail.com>
2023-04-22 13:10:18 +0300

    Rewriting urls using the htaccess file

--- Projects/notes/app/core/App.php
+++ Projects/notes/app/core/App.php
@@ -3,16 +3,16 @@
 class App {
 
 
-    // protected $controller = 'home';
-    // protected $method = 'index';
-    // protected $params = [ ];
-    // public function __construct() {
-    //     $this->parseUrl();
-    // }
+    protected $controller = 'home';
+    protected $method = 'index';
+    protected $params = [ ];
+    public function __construct() {
+        $this->parseUrl();
+    }
 
-    // public function parseUrl() {
-    //     if ( isset($_GET['url'])) {
-    //         echo $_GET['url'];
-    //     }
-    // }
+    public function parseUrl() {
+        if ( isset($_GET['url'])) {
+            echo $_GET['url'];
+        }
+    }
 }
