41b07fb86ff89a159a988b182eb6aa0227a839e2
David Derrick Anyuru <davidderrickanyuru@gmail.com>
2023-04-22 21:14:56 +0300

    Views and Database connection

--- Projects/notes/app/controllers/home.php
+++ Projects/notes/app/controllers/home.php
@@ -2,7 +2,10 @@
 
 class Home extends Controller {
     
-    public function index($name = '', $otherName = '') {
-        echo $name. ' ' .$otherName;
+    public function index($name = '') {
+        $user = $this->model('User');
+        $user->name = $name;
+        
+        $this->view('home/index', ['name' => $user->name]);
     }
 }
--- Projects/notes/app/core/App.php
+++ Projects/notes/app/core/App.php
@@ -28,7 +28,6 @@ class App {
 
     public function parseUrl() {
         if ( isset($_GET['url'])) {
-            // echo $_GET['url'];
             return explode('/', filter_var(rtrim($_GET['url'], '/'), FILTER_SANITIZE_URL));
         }
     }
